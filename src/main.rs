15d0699f2db505d706159060b6e2a189a85ebbf8
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2018-07-14 23:46:20 +0200

    Moved to hyper

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,57 @@
-#![feature(trace_macros)]
-#[macro_use]
 extern crate futures;
-#[macro_use]
-extern crate nom;
+extern crate hyper;
+extern crate rand;
 extern crate tokio;
 
 use futures::prelude::*;
 
-use tokio::io;
-use tokio::net::TcpListener;
-use tokio::prelude::*;
 
-mod http_parser;
-/*
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::service::Service;
 
-struct RequestReader<'a, T> {
-    inner: T,
-    headers: [Header<'a>; 256], // Maximum number of headers
-    buf: [u8; 1024 * 16],       // Maximum size of all headers
-    position: usize,
+use rand::Rng;
+
+struct ClientSession {
+    id: u64,
 }
 
-struct ReadRequest<'a> {}
-
-fn hreader<'a, T>(stream: T) -> RequestReader<'a, T> {
-    RequestReader {
-        inner: stream,
-        headers: Box::new([]),
-        buf: [0; 4096],
-        position: 0,
+fn new_session() -> ClientSession {
+    ClientSession {
+        id: rand::thread_rng().gen_range(0, 2 ^ 64 - 1),
     }
 }
 
-impl<'a, T: AsyncRead> Future for RequestReader<'a, T> {
-    type Item = RequestReader<'a, T>;
-    type Error = io::Error;
+impl Service for ClientSession {
+    // this is the body gives you
+    type ReqBody = hyper::Body;
+    // you could change this to a custom `Payload` impl if you wanted
+    type ResBody = hyper::Body;
+    // can be any `E: std::error::Error`
+    type Error = String;
+    // doesn't have to Box<Future>, it's just easier if you return different
+    // future types depending on conditions
+    type Future = Box<Future<Item = Response<Self::ResBody>, Error = Self::Error> + Send>;
 
-    fn poll(&mut self) -> Result<Async<Self::Item>, Self::Error> {
-        let read = try_ready!(self.inner.poll_read(&mut self.buf[self.position..]));
-        self.position += read;
-        // TODO: header size limit
-        // TODO: error handling
-        match parse_headers(&self.buf[0..self.position], &mut self.headers).unwrap() {
-            httparse::Status::Complete(_) => Ok(Async::Ready(self)),
-            httparse::Status::Partial => Ok(Async::NotReady),
-        }
+    fn call(&mut self, req: Request<Self::ReqBody>) -> Self::Future {
+        handle_request(self, req)
     }
+}
+
+fn handle_request(
+    session: &ClientSession,
+    req: Request<Body>,
+) -> Box<Future<Item = Response<Body>, Error = String> + Send> {
+    Box::new(futures::done(Ok(
+        Response::new(Body::from(format!("Session {}", session.id))),
+    )))
 }
 
 fn main() {
-    // Bind the server's socket
-    let addr = "10.0.0.2:3000".parse().unwrap();
-    let tcp = TcpListener::bind(&addr).unwrap();
+    let addr = ([10, 0, 0, 2], 3000).into();
 
-    // Iterate incoming connections
-    let server = tcp.incoming()
-        .for_each(|tcp| {
-            // Split up the read and write halves
-            let (reader, writer) = tcp.split();
+    let new_service = || Ok::<_, String>(new_session());
+    let server = Server::bind(&addr).serve(new_service);
 
-            // Copy the data back to the client
-            let conn = io::copy(reader, writer)
-            // print what happened
-            .map(|(n, _, _)| {
-                println!("wrote {} bytes", n)
-            })
-            // Handle any errors
-            .map_err(|err| {
-                println!("IO error {:?}", err)
-            });
-
-            // Spawn the future as a concurrent task
-            tokio::spawn(conn);
-
-            Ok(())
-        })
-        .map_err(|err| {
-            println!("server error {:?}", err);
-        });
-
-    // Start the runtime and spin up the server
-    tokio::run(server);
+    println!("Listening on http://{}", addr);
+    hyper::rt::run(server.map_err(|err| eprintln!("server error: {}", err)));
 }
-*/
-
-fn main() {}
